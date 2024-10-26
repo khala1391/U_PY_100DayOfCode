783f1b6450e44d92f178a1dd4dd835d056cf7642
khala1391 <khala1391@gmail.com>
2024-10-26 22:17:43 +0700

    update

--- day32/quote/main_.py
+++ day32/quote/main_.py
@@ -19,7 +19,7 @@ if weekday == 5:
         connection.starttls()
         connection.login(user=MY_EMAIL,password=MY_PASSWORD)
         connection.sendmail(from_addr=MY_EMAIL,
-                            to_addrs="khala1391@hotmail.com",
+                            to_addrs="k@hotmail.com",
                             msg= f"Subject: Monday Motivation \n\n {quote}")
     
     
